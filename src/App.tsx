efc674f9f217383011898a2afa59ba3d7453ad1c
Saad UR Rehman <saadii.github@gmail.com>
2026-01-21 22:39:47 +0500

    Added responsive layout

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
+
+function App() {
+  return (
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`
+    }}>
+      <GridItem area='nav' bg={'red'}>Nav</GridItem>
+      <GridItem area='nav' bg={'red'}>Nav</GridItem>
+      <Show above='lg'>
+        <GridItem area='aside ' bg={'green'}>Aside</GridItem>
+      </Show>
+      <GridItem area='main' bg={'blue'}>Main</GridItem>
+
+    </Grid>
+  );
+}
+
+export default App;
