51a5308b145932dd19b81fd5afa3efbec741d198
Saad UR Rehman <saadii.github@gmail.com>
2026-01-21 23:07:23 +0500

    Added a darkmode toggle

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,14 @@
 import { HStack, Image, Text } from "@chakra-ui/react"
+import ColorModeSwitch from './ColorModeSwitch'
 import logo from '../assets/react.svg'
 
 const Navbar = () => {
     return (
-        <HStack>
+        <HStack justifyContent={'space-between'} padding={'10px'}>
             <Image src={logo} boxSize='60px'></Image>
             <Text>Navbar</Text>
-        </HStack>
+            <ColorModeSwitch />
+        </HStack >
     )
 }
 
