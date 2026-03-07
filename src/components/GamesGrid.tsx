ca4cebef6e856e460086e4cba644f485dd774b19
Saad UR Rehman <saadii.github@gmail.com>
2026-03-07 20:54:18 +0500

    Impoved ux by adding skeletons during loading.

--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -2,9 +2,11 @@ import { SimpleGrid } from "@chakra-ui/react";
 
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GamesGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, loading } = useGames();
+  const gameCardSekeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
     <>
@@ -14,9 +16,9 @@ const GamesGrid = () => {
         spacing={10}
         padding={3}
       >
-        {games.map((game) => (
-          <GameCard key={game.id} game={game} />
-        ))}
+        {loading
+          ? gameCardSekeletons.map((gcs) => <GameCardSkeleton key={gcs} />)
+          : games.map((game) => <GameCard key={game.id} game={game} />)}
       </SimpleGrid>
     </>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -25,23 +25,30 @@ interface RawgIoResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [loading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setIsLoading(true);
+
     apiClient
       .get<RawgIoResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setIsLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
 
         setError(err.message);
+        setIsLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, loading };
 };
 
 export default useGames;
